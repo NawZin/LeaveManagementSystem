eaa6d17befb8b7e0afaaaa06ce843e145a689630
Marlar-PC <Marlar-PC@DESKTOP-TFNIBRI>
2024-07-04 19:07:52 +0900

    Add project files.

---
+++ LeaveManagementSystem.Web/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using LeaveManagementSystem.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagementSystem.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = new TestViewModel
+            {
+                Name = "Aung Aung",
+                DateOfBirth = new DateTime(1989,08,22)
+            };
+            return View(data);
+        }
+    }
+}

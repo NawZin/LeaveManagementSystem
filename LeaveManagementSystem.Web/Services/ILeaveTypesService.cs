4ef843d226eac4de7b28720ea277a1b2cc9e3f50
Marlar-PC <Marlar-PC@DESKTOP-TFNIBRI>
2024-07-10 03:53:11 +0900

    Implementing Service Layer

---
+++ LeaveManagementSystem.Web/Services/ILeaveTypesService.cs
@@ -0,0 +1,16 @@
+﻿using LeaveManagementSystem.Web.Models.LeaveTypes;
+
+namespace LeaveManagementSystem.Web.Services
+{
+    public interface ILeaveTypesService
+    {
+        Task<List<LeaveTypeReadOnlyVM>> GetAll();
+        Task Create(LeaveTypeCreateVM model);
+        Task Remove(int id);
+        Task Edit(LeaveTypeEditVM model);
+        Task<T?> Get<T>(int id) where T : class;
+        bool LeaveTypeExists(int id);
+        Task<bool> CheckIfLeaveTypeExists(string name);
+        Task<bool> CheckIfLeaveTypeExistsForEdit(LeaveTypeEditVM leaveTypeEdit);
+    }
+}
